23b7b398e84e88b4fec44a36a2e2191fa40f5ac7
Johnathan <johnathan.mnn@gmail.com>
2022-07-14 08:21:05 -0500

    9am time block working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,14 +12,14 @@ console.log(currentTime);
 
 // These functions can be called to save user input
 var store= function(){
-    var text = document.getElementById("text8AM").value;
+    var text = $("#text8AM").value;
     localStorage.setItem("text8AM",text);
 }
     //local storage to keep values after refresh
 var getValue = function(){
     var storedText = localStorage.getItem("text8AM");
     if(storedText != null){
-    document.getElementById("text8AM").value = storedText; 
+    $("#text8AM").value = storedText; 
     }
 }
 
@@ -46,12 +46,43 @@ if(currentTime < 8){
 // Save 8am user input
 
 $('#btn8AM').click(function () {
-    var text = document.getElementById("text8AM").value;
+    var text = $("#text8AM").value;
     localStorage.setItem("text8AM",text);
     });
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("text8AM").onload = getValue();
+    $("#text8AM").onload = getValue();
+});
+
+// Set 9am status
+// If 9am is in the past
+if(currentTime > 9){
+    $('#row9AM').addClass('past');
+    $('#time9AM').addClass('past_hour');
+    $('#btn9AM').addClass('past_btn');
+}
+// If 9am is the current time
+if(currentTime === 9){
+    $('#row9AM').addClass('present');
+    $('#time9AM').addClass('present_hour');
+    $('#btn9AM').addClass('present_btn');
+}
+// If 9am is in the future
+if(currentTime < 9){
+    $('#row9AM').addClass('future');
+    $('#time9AM').addClass('future_hour');
+    $('#btn9AM').addClass('future_btn');
+}
+
+// Save 9am user input
+
+$('#btn9AM').click(function () {
+    var text = $("#text9AM").value;
+    localStorage.setItem("text9AM",text);
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+    $("#text9AM").onload = getValue();
 });
 
 
