a17618853359b8f673449e9127e8dfb21bf1341e
Johnathan <johnathan.mnn@gmail.com>
2022-07-13 22:18:02 -0500

    Text input now works and saves

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,27 @@
 // Display current date and time
 var update = function() {
-    $("#currentDay")
+    document.getElementById("datetime")
     .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
 setInterval(update, 1000);
 
 // Find current hour
 var currentTime = moment().hour();
+
+console.log(currentTime);
+
+// These functions can be called to save user input
+var store= function(){
+    var text = document.getElementById("text8AM").value;
+    localStorage.setItem("text8AM",text);
+}
+    //local storage to keep values after refresh
+var getValue = function(){
+    var storedText = localStorage.getItem("text8AM");
+    if(storedText != null){
+    document.getElementById("text8AM").value = storedText; 
+    }
+}
 
 // Set 8am status
 // If 8am is in the past
@@ -16,7 +31,7 @@ if(currentTime > 8){
     $('#btn8AM').addClass('past_btn');
 }
 // If 8am is the current time
-if(currentTime = 8){
+if(currentTime === 8){
     $('#row8AM').addClass('present');
     $('#time8AM').addClass('present_hour');
     $('#btn8AM').addClass('present_btn');
@@ -28,11 +43,16 @@ if(currentTime < 8){
     $('#btn8AM').addClass('future_btn');
 }
 
+// Save 8am user input
 
+$('#btn8AM').click(function () {
+    var text = document.getElementById("text8AM").value;
+    localStorage.setItem("text8AM",text);
+    });
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text8AM").onload = getValue();
+});
 
-// if(currentTime >= 8){
-//     eightAm.addClass('past');
-// }
 
 
