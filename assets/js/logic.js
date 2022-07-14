359b59b8b5ec6a322686fe5ab50378959bb4b9f5
Johnathan <johnathan.mnn@gmail.com>
2022-07-14 08:48:21 -0500

    10AM block working and save error fixes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,20 +8,12 @@ setInterval(update, 1000);
 // Find current hour
 var currentTime = moment().hour();
 
-console.log(currentTime);
 
-// These functions can be called to save user input
-var store= function(){
-    var text = $("#text8AM").value;
-    localStorage.setItem("text8AM",text);
-}
-    //local storage to keep values after refresh
-var getValue = function(){
-    var storedText = localStorage.getItem("text8AM");
-    if(storedText != null){
-    $("#text8AM").value = storedText; 
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text8AM").onload = getValue8AM();
+    document.getElementById("text9AM").onload = getValue9AM();
+    document.getElementById("text10AM").onload = getValue10AM();
+});
 
 // Set 8am status
 // If 8am is in the past
@@ -43,16 +35,24 @@ if(currentTime < 8){
     $('#btn8AM').addClass('future_btn');
 }
 
-// Save 8am user input
+// Allows information to be loaded on reload
+var store8AM= function(){
+    var text = document.getElementById("text8AM").value;
+    localStorage.setItem("text8AM",text);
+}
 
+var getValue8AM = function(){
+    var storedText = localStorage.getItem("text8AM");
+    if(storedText != null){
+    document.getElementById("text8AM").value = storedText; 
+    }
+} 
+// Save 8am user input
 $('#btn8AM').click(function () {
-    var text = $("#text8AM").value;
+    var text = document.getElementById("text8AM").value;
     localStorage.setItem("text8AM",text);
     });
 
-document.addEventListener("DOMContentLoaded", function() {
-    $("#text8AM").onload = getValue();
-});
 
 // Set 9am status
 // If 9am is in the past
@@ -74,16 +74,61 @@ if(currentTime < 9){
     $('#btn9AM').addClass('future_btn');
 }
 
-// Save 9am user input
+// Allows information to be loaded on reload
+var store9AM= function(){
+    var text = document.getElementById("text9AM").value;
+    localStorage.setItem("text9AM",text);
+}
 
+var getValue9AM = function(){
+    var storedText = localStorage.getItem("text9AM");
+    if(storedText != null){
+    document.getElementById("text9AM").value = storedText; 
+    }
+} 
+// Save 9am user input
 $('#btn9AM').click(function () {
-    var text = $("#text9AM").value;
+    var text = document.getElementById("text9AM").value;
     localStorage.setItem("text9AM",text);
     });
 
-document.addEventListener("DOMContentLoaded", function() {
-    $("#text9AM").onload = getValue();
-});
+// Set 9am status
+// If 9am is in the past
+if(currentTime > 10){
+    $('#row10AM').addClass('past');
+    $('#time10AM').addClass('past_hour');
+    $('#btn10AM').addClass('past_btn');
+}
+// If 10am is the current time
+if(currentTime === 10){
+    $('#row10AM').addClass('present');
+    $('#time10AM').addClass('present_hour');
+    $('#btn10AM').addClass('present_btn');
+}
+// If 10am is in the future
+if(currentTime < 10){
+    $('#row10AM').addClass('future');
+    $('#time10AM').addClass('future_hour');
+    $('#btn10AM').addClass('future_btn');
+}
+
+// Allows information to be loaded on reload
+var store10AM= function(){
+    var text = document.getElementById("text10AM").value;
+    localStorage.setItem("text10AM",text);
+}
+
+var getValue10AM = function(){
+    var storedText = localStorage.getItem("text10AM");
+    if(storedText != null){
+    document.getElementById("text10AM").value = storedText; 
+    }
+} 
+// Save 9am user input
+$('#btn10AM').click(function () {
+    var text = document.getElementById("text10AM").value;
+    localStorage.setItem("text10AM",text);
+    });
 
 
 
